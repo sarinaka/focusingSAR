1ce5fefd6c0443904ad2a520531987464b058330
Paul Summers <psummers@stanford.edu>
2020-04-16 15:58:09 -0700

    update code comments and titles to plot

--- chirpOut.m
+++ chirpOut.m
@@ -1,6 +1,7 @@
 function [Y] = chirpOut(X,t,r,theta,f_c)
-% [Y] = chirpOut(X,t,r,theta) Returns the raw (Y) data
-% from an input chirp (X,t) off a point scattered range (r) away at angle (theta).
+% [Y] = chirpOut(X,t,r,theta,f_c) Returns the raw (Y) data
+% from an input chirp (X,t,f_c) off a point scattered range (r) away at angle
+% (theta). Chirp (X), time vector(t), center freq (f_c).
 c = 3e8;
 lambda_c = c/f_c;
 delay = r/c;
