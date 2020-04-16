fb5d3ef7fe279be17f2c4aebed3f216380e65a87
Paul Summers <psummers@stanford.edu>
2020-04-16 15:34:23 -0700

    introduce phase adjustment to chirp traveling, around center frequency of chirp is an approx

--- chirpOut.m
+++ chirpOut.m
@@ -1,8 +1,9 @@
-function [Y] = chirpOut(X,t,r,theta)
-% [Y,Y_MF] = chirpOut(X,r) Returns the raw (Y) and Match filtered (Y_MF) data
+function [Y] = chirpOut(X,t,r,theta,f_c)
+% [Y] = chirpOut(X,t,r,theta) Returns the raw (Y) data
 % from an input chirp (X,t) off a point scattered range (r) away at angle (theta).
-delay = r/3e8;
-
+c = 3e8;
+lambda_c = c/f_c;
+delay = r/c;
 amp = 1; 
 % amp = exp(-r/1e7);
-Y = amp*interp1(t,X,t-delay,'pchip',0);
+Y = amp*interp1(t,X,t-delay,'pchip',0).*exp(1i.*(2.*pi*r./lambda_c));
