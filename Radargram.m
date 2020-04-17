86176ed439a4a3bb91fafa9e55de9208297eba69
Paul Summers <psummers@stanford.edu>
2020-04-16 20:16:47 -0700

    cleaning up, comments and adjust to real ice geometry

--- Radargram.m
+++ Radargram.m
@@ -1,63 +1,69 @@
-% Make n chirps while moving btwn chirps, try making a radargram
-
+% This is a script to make n chirps from stationary platform, while moving 
+% between chirps to make a scene. Produce both raw data and match filtered
+% data products
 clear
-% Rx
-F_s = 3e4;            % Sampling frequency [Hz]
-T   = .1;              %Record Time [s];
+%% Inputs
+% Rx parameters
+F_s = 1e7;            % Sampling frequency [Hz]
+T   = 3e-4;              %Record Time [s];
 t = 0:1/F_s:T;       % Time vector [s]
-L = length(t);      % Recording length [ ]
-% Tx
-f_c = 1e2;            % Intial frequency of chirp [Hz] 
-swp = 1.5e5;           % Sweep frequency of chirp [Hz/s]
-t_c = 1e-2;            % Chirp Length [s]
+L = length(t);      % Recording vector length [ ]
+% Tx parameters
+f_c = 2e6;            % Intial frequency of chirp [Hz] 
+swp = 2e6;           % Sweep frequency of chirp [Hz/s]
+t_c = 1e-5;            % Chirp Length [s]
+% Survey Parameters
+n = 101; %surface sample points
+dx = 1e3; % Distance between sample points [m]
 
-n = 100; %surface sample points
-Y = zeros(n,L);
-Ymf = Y;
+%% Make pulse
+t_sub = 0:1/F_s:t_c; %pulse only for duration of pulse
+X = zeros(size(t));  %pulse signal is 0 otherwise
+X(1:length(t_sub)) = exp(1i*(pi.*swp.*t_sub.^2+2.*pi.*f_c.*t_sub)); %LFMCM pulse
 
-t_sub = 0:1/F_s:t_c;
-X = zeros(size(t));
-X(1:length(t_sub)) = exp(1i*(pi.*swp.*t_sub.^2+2.*pi.*f_c.*t_sub));
-
-%sweep along surface, let full pulse out and wait for return, then move.
+%% Sweep along surface, let full pulse out and wait for return, then move.
+Y = zeros(n,L); %initialize raw data field
+Ymf = Y;        %initialize match filtered data field
+R = zeros(n,1);
 for i = 1:n
-    %Find range, shift chirp
-    r = sqrt((5e6).^2 + ((i-50)*2e5).^2);
+    %Find range, shift chirp (in Time and Fx)
+    r = sqrt((4e3).^2 + ((i-50)*dx).^2);
+    R(i) = r;
     y_tmp = chirpOut(X,t,r,0,f_c);
     
     %raw data
     Y(i,:) = y_tmp; 
     
-    % pulse compress in range, this could be done in a separate processing
+    % match filter in range, this could be done in a separate processing
     % loop if we wanted, indepentent to data collection
     Ymf(i,:) = ifft( fft(y_tmp)  .* conj( fft(X) ) );
 end
 
 %% Plot the returns, abs() of complex values to display
-% figure(1) %Faster plotting option, but can't see waves
-% clf
-% subplot(211)
-% imagesc(abs(Y'))
-% ylabel('range')
-% xlabel('along track')
-% title('Raw data')
-% colorbar
-% subplot(212)
-% imagesc(abs(Ymf'))
-% ylabel('range')
-% xlabel('along track')
-% title('Match Filtered data')
-% colorbar
-
-figure(1) %very slow plotting, but cool to see waveforms!
+figure(1) %Faster plotting option, but can't see waves
 clf
 subplot(211)
-wiggle(real(Y'))
+imagesc(abs(Y'))
 ylabel('range')
 xlabel('along track')
 title('Raw data')
+colorbar
 subplot(212)
-wiggle(abs(Ymf'))
+imagesc(abs(Ymf'))
 ylabel('range')
 xlabel('along track')
-title('Range Match Filtered data')
+title('Match Filtered data')
+colorbar
+
+% figure(2) %very slow plotting, but cool to see waveforms!
+% clf
+% subplot(211)
+% wiggle(real(Y'))
+% ylabel('range')
+% xlabel('along track')
+% title('Raw data')
+% subplot(212)
+% wiggle(abs(Ymf'))
+% ylabel('range')
+% xlabel('along track')
+% title('Range Match Filtered data')
