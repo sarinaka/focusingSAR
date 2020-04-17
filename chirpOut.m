9a750fa039bb8fccf094acd8225eeb6d596982de
Paul Summers <psummers@stanford.edu>
2020-04-16 20:17:41 -0700

    clean up sections, adjust for ice index of refraction

--- chirpOut.m
+++ chirpOut.m
@@ -2,9 +2,15 @@ function [Y] = chirpOut(X,t,r,theta,f_c)
 % [Y] = chirpOut(X,t,r,theta,f_c) Returns the raw (Y) data
 % from an input chirp (X,t,f_c) off a point scattered range (r) away at angle
 % (theta). Chirp (X), time vector(t), center freq (f_c).
-c = 3e8;
+c = 3e8/1.31;
+
+%% Phase delay
 lambda_c = c/f_c;
 delay = r/c;
+
+%% Attenuation
 amp = 1; 
 % amp = exp(-r/1e7);
+
+%% Shift in space, apply phase and attenuation
 Y = amp*interp1(t,X,t-delay,'pchip',0).*exp(1i.*(2.*pi*r./lambda_c));
