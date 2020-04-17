d674ef05f7f1857d5ae76900b7c6c72088dded4a
Paul Summers <psummers@stanford.edu>
2020-04-16 20:44:14 -0700

    add noise option into chirpOut with rand amp and phase

--- chirpOut.m
+++ chirpOut.m
@@ -12,5 +12,9 @@ delay = r/c;
 amp = 1; 
 % amp = exp(-r/1e7);
 
+%% Noise, if you want it
+SNR = 2;
+noise = randn(size(X)).*exp(1i*rand(size(X))*2*pi); %Rand amp (normal), rand phase (uniform)
+% noise = zeros(size(X));  %b/c dividing by 0 is hard, use this for 0 noise
 %% Shift in space, apply phase and attenuation
-Y = amp*interp1(t,X,t-delay,'pchip',0).*exp(1i.*(2.*pi*r./lambda_c));
+Y = amp*interp1(t,X,t-delay,'pchip',0).*exp(1i.*(2.*pi*r./lambda_c)) + noise./SNR;
