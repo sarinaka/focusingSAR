1b72eaad5b51efe800b0116a84524e2c1f1a55ae
Paul Summers <psummers@stanford.edu>
2020-04-30 13:59:03 -0700

    modify for more easily adjusted output

--- Paul/processBlock.m
+++ Paul/processBlock.m
@@ -1,5 +1,5 @@
-function [I] = processBlock(Y,X,f_s,f_c,dx,t)
-% [I] = processBlock(Y,X,f_s,f_c,dx) take raw data Y and processed for 
+function [Imf] = processBlock(Y,X,f_s,f_c,dx,t)
+% [I] = processBlock(Y,X,f_s,f_c,dx,t) take raw data Y and processed for 
 % chirp X centered at middle of block. Blocks must be odd in width.
 c = 3e8/1.31; %speed of light in ice
 n = size(Y,1); %Width
@@ -10,14 +10,19 @@ lambda_c = c/f_c; %Wavelength of center freq of chirp
 %% Match filter in fast time
 Imf = ifft( fft(Y,[],2) .* conj(fft(X)) ,[],2);
 
-%% Need to shift into correct range bin
+%% Need to shift into correct range bin (optimized for point scatterer)
+% Create interpolant to migrate hyperbolas back to same range bin, this
+% only works well for scatterers in center of block and Depth >> Apeture
 [x,y] = ndgrid(xx,t*c/2);
 Rshift = griddedInterpolant(x,y,Imf);
-Imfsh = Rshift(x,t*c/2+x.^2./(2*t*c/2));
+% Interpolate signal back up
+Imf = Rshift(x,t*c/2+x.^2./(2*t*c/2));
 
-%% Match filter in Az
+%% Match filter in Az (optimized for point scatterer)
+% Generate shift to center of frame
 w = exp(-1i * 2 * pi * (n-1)/2 * [0:floor(n/2)-1 floor(-n/2):-1]'/ n); 
+% Generate expected phase shift
 rr = sqrt(xx.^2 + ((1:L)*(1/f_s)*c/2).^2);
 az = exp(1i.*(2.*pi*(2*rr)./lambda_c));
-
-I = ifft( fft(Imfsh,[],1)  .* conj( fft(az,[],1) ) .* w,[],1);
+% Match filter across Az, shift signal matches to center
+Imf = ifft( fft(Imf,[],1)  .* conj( fft(az,[],1) ) .* w,[],1);
