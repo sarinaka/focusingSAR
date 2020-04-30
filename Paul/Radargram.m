4577f83168d27629111c202313490d35c1e99477
Paul Summers <psummers@stanford.edu>
2020-04-29 20:38:10 -0700

    range adjustment now included in process block

--- Paul/Radargram.m
+++ Paul/Radargram.m
@@ -4,28 +4,29 @@
 clear
 %% Inputs
 % Rx parameters
-f_s = 120e6;            % Sampling frequency [Hz]
+f_s = 60e6;          % Sampling frequency [Hz]
 T   = 5e-5;           % Record Time [s];
 t = 0:1/f_s:T;        % Time vector [s]
 L = length(t);        % Recording vector length [ ]
 % Tx parameters
-f_0 = 50e6;           % Intial frequency of chirp [Hz] 
-swp = 50e6;           % BW of chirp [Hz]
+f_0 = 20e6;           % Intial frequency of chirp [Hz] 
+swp = 40e6;           % BW of chirp [Hz]
 f_c = f_0 + swp/2;    % Center frequency [Hz]
            
 t_c = 1e-5;           % Chirp Length [s]
 % Survey Parameters
 c = 3e8/1.31;
-lambda_c = c/f_c;% Wavelength in ice [m]
-lambda_0 = c/f_0;
-n = 1001; 			  %surface sample points
-dx = 200/1001;  	  % Distance between sample points [m]
+lambda_c = c/f_c;     % Wavelength (center) in ice [m]
+lambda_0 = c/f_0;     % Wavelength (initial) in ice [m]
+n = 1001; 			  % surface sample points
+dx = 800/1001;  	  % Distance between sample points [m]
+dy = (1/f_s)*c/2;     % Distance of 1 way dist per range bin [m]
 SynthAx = n*dx;       % Synthetic Apeture [m]
 xx = (([1:n]*dx)-(n+1)/2*dx)';
 depth = 4e3;          % Scatter Depth [m]
 % Plotting params
 delay_index = round(depth*2/c*f_s);
-i_range = 50;
+i_range = 100;
 i_min = delay_index-i_range;
 i_max = delay_index+i_range;
 
@@ -41,8 +42,8 @@ R = zeros(n,1);
 for i = 1:n
     %Find range, shift chirp (in Time and Fx)
     r1 = sqrt((depth).^2 + ((i-500)*dx).^2);
-%     r2 = sqrt((depth+1e2).^2 + ((i-250)*dx).^2);
-    y_tmp = chirpOut(X,t,r1,0,f_c,f_s); % + chirpOut(X,t,r2,0,f_c,f_s);
+    r2 = sqrt((depth+4e1).^2 + ((i-450)*dx).^2);
+    y_tmp = chirpOut(X,t,r1,0,f_c,f_s) + chirpOut(X,t,r2,0,f_c,f_s);
     
     %raw data
     Y(i,:) = y_tmp; 
@@ -61,20 +62,26 @@ for i = 1:n
     clear r y_tmp
 end
 
+[x,y] = ndgrid(xx,t*c/2);
+Rshift = griddedInterpolant(x,y,Ymf);
+Ymfsh = Rshift(x,t*c/2+x.^2./(2*t*c/2));
+
+
+
 %% Azimuth Processing
 Ymfaz = zeros(n,i_range*2+1);
 %shift in reflector to our domain
 w = exp(-1i * 2 * pi * (n-1)/2 * [0:floor(n/2)-1 floor(-n/2):-1]'/ n); 
 for j = 1:L
     rr = sqrt(xx.^2 + ((j)*(1/f_s)*c/2)^2);
-    z = Ymf(:,j);
+    z = Ymfsh(:,j);
     az = exp(1i.*(2.*pi*(2*rr)./lambda_c));
     Ymfaz(:,j) = ifft( fft(z)  .* conj( fft(az) ) .* w);
 end
 
 toc
 
-I = processBlock(Y,X,f_s,f_c,dx); %Processing Az with initial freq (not center) works way better
+I = processBlock(Y,X,f_s,f_c,dx,t); %Processing Az with initial freq (not center) works way better
 
 %% Plot the returns, abs() of complex values to display
 figure(1) %Faster plotting option, but can't see waves
@@ -86,7 +93,7 @@ subplot(311)
 	title('Raw data')
 	colorbar
 subplot(312)
-	prettyPlot(abs(Ymf(:,i_min:i_max)'))
+	prettyPlot(abs(Ymfsh(:,i_min:i_max)'))
 	ylabel('range')
 	xlabel('along track')
 	title('Match Filtered data')
@@ -120,37 +127,51 @@ subplot(313)
 
 %% Azimuth Focusing play area
 
-% figure(3)
-% clf
-% subplot(311)
-% plot(real(Y(:,delay_index:i_max)))
-% title('azimuth real data at reflector')
-% legend
+i_max = delay_index + 3;
+z = Ymf(:,delay_index:i_max);
+
+% Generate 2-way distance to reflector at this bin
+rr = sqrt(xx.^2 + ((delay_index:i_max)*(1/f_s)*c/2).^2);
+% Generate phase delay at this distance along Az to filter with
+az = exp(1i.*(2.*pi*(2*rr)./lambda_c));
+% shift the signal to be centered mid frame
+w = exp(-1i * 2 * pi * (n-1)/2 * [0:floor(n/2)-1 floor(-n/2):-1]'/ n); 
+
+
+figure(3)
+clf
+subplot(411)
+plot(real(Y(:,delay_index:i_max)))
+title('azimuth real data at reflector')
+legend
+
+subplot(412)
+plot(real(Ymf(:,delay_index:i_max)))
+title('azimuth fast time match filtered real data at reflector')
+legend
+
+subplot(413)
+plot(real(az))
+title('expected real signal in Az')
+legend
+if mod(n, 2) == 0
+	% Force conjugate symmetry. Otherwise this frequency component has no
+	% corresponding negative frequency to cancel out its imaginary part.
+	w(n/2+1) = real(w(n/2+1));
+end 
+Zmf = ifft( fft(z)  .* conj( fft(az) ) .* w);
+
+subplot(414)
+plot(abs(Zmf))
+legend
 % 
 % figure(4)
 % clf
 % plot((R-depth)/lambda_c)
 % title('Range change in units of wavelength')
-% 
-% 
-% z = Ymf(:,delay_index:i_max);
-% rr = 2 * sqrt(xx.^2 + ((delay_index:i_max)*(1/f_s)*c/2).^2);
-% az = exp(1i.*(2.*pi*2*rr./lambda_0));
-% w = exp(-1i * 2 * pi * (n-1)/2 * [0:floor(n/2)-1 floor(-n/2):-1]'/ n); 
-% 
-% 
-% subplot(312)
-% 
-% plot(real(az))
-% title('expected phase shift')
-% legend
-% if mod(n, 2) == 0
-% 	% Force conjugate symmetry. Otherwise this frequency component has no
-% 	% corresponding negative frequency to cancel out its imaginary part.
-% 	w(n/2+1) = real(w(n/2+1));
-% end 
-% Zmf = ifft( fft(z)  .* conj( fft(az) ) .* w);
-% 
-% subplot(313)
-% plot(abs(Zmf))
-% legend
+
+
+
+
+
+
--- Paul/processBlock.m
+++ Paul/processBlock.m
@@ -1,4 +1,4 @@
-function [I] = processBlock(Y,X,f_s,f_c,dx)
+function [I] = processBlock(Y,X,f_s,f_c,dx,t)
 % [I] = processBlock(Y,X,f_s,f_c,dx) take raw data Y and processed for 
 % chirp X centered at middle of block. Blocks must be odd in width.
 c = 3e8/1.31; %speed of light in ice
@@ -7,10 +7,17 @@ L = size(Y,2); %Time recording length
 xx = (((1:n)*dx)-(n+1)/2*dx)'; %lenth vector in azimuth
 lambda_c = c/f_c; %Wavelength of center freq of chirp
 
+%% Match filter in fast time
 Imf = ifft( fft(Y,[],2) .* conj(fft(X)) ,[],2);
 
+%% Need to shift into correct range bin
+[x,y] = ndgrid(xx,t*c/2);
+Rshift = griddedInterpolant(x,y,Imf);
+Imfsh = Rshift(x,t*c/2+x.^2./(2*t*c/2));
+
+%% Match filter in Az
 w = exp(-1i * 2 * pi * (n-1)/2 * [0:floor(n/2)-1 floor(-n/2):-1]'/ n); 
 rr = sqrt(xx.^2 + ((1:L)*(1/f_s)*c/2).^2);
 az = exp(1i.*(2.*pi*(2*rr)./lambda_c));
 
-I = ifft( fft(Imf,[],1)  .* conj( fft(az,[],1) ) .* w,[],1);
+I = ifft( fft(Imfsh,[],1)  .* conj( fft(az,[],1) ) .* w,[],1);
